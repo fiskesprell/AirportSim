ab386bd2df5fa6bf1b95c44fe1c0e932884f5f2b
fiskesprell <jo.hovet@hotmail.com>
2024-01-24 14:50:28 +0100

    Added instance vars and constructor to airport, runway, terminal

--- AirportSimulation/Airport.cs
+++ AirportSimulation/Airport.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,13 @@ namespace AirportSimulation
 {
     internal class Airport
     {
+        // Instance Variables
+        private string AirportName;
+
+        // Constructor
+        public Airport(string AirportName)
+        {
+            this.AirportName = AirportName;
+        }
     }
 }
