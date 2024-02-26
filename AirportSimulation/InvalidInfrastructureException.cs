a8331ebfdae1abdb388b05e91bd36be2fab4fe67
Vebhamnvik <vebjornhamnvik95@gmail.com>
2024-02-26 15:15:55 +0100

    Laget InvalidInfra.... og InvalidSched.....Exceptions. Laget et skjellet av klassene.

---
+++ AirportSimulation/InvalidInfrastructureException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportSimulationCl
+{
+    public class InvalidInfrastructureException : Exception
+    {
+        public InvalidInfrastructureException() : base() { }
+
+        public InvalidInfrastructureException(string message) : base(message) { }
+
+        public override string Message
+        {
+            get
+            {
+                return $"InvalidInfrastructureException: {base.Message}";
+            }
+        }
+    }
+}
---
+++ AirportSimulation/InvalidScheduledTimeException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportSimulationCl
+{
+    public class InvalidScheduledTimeException : Exception
+    {
+        public InvalidScheduledTimeException() : base() { }
+
+        public InvalidScheduledTimeException(string message) : base(message) { }
+
+        public override string Message
+        {
+            get
+            {
+                return $"InvalidScheduledTimeException: {base.Message}";
+            }
+        }
+    }
+}
