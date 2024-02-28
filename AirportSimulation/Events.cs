b4dc3e91fca207688208d9eb963d6fff412c673b
fiskesprell <jo.hovet@hotmail.com>
2024-02-28 20:09:10 +0100

    Laget events.cs

---
+++ AirportSimulation/Events.cs
@@ -0,0 +1,26 @@
+﻿using AirportSimulation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportSimulationCl
+{
+    internal class Events
+    {
+        // Delegate
+        public delegate void FlightEventHandler(Flight Flight, Plane plane);
+
+        // Events (ingen håndtering enda)
+        public event FlightEventHandler? FlightGetsAssignedGate;
+        public event FlightEventHandler? FlightGetsAssignedTaxi;
+        public event FlightEventHandler? FlightGetsAssignedRunway;
+        public event FlightEventHandler? FlightHasLeftRunway;
+        public event FlightEventHandler? FlightHasLanded;
+        public event FlightEventHandler? FlightHasBegunOnboarding;
+        public event FlightEventHandler? FlightHasBegunOffloading;
+        
+
+    }
+}
